2a7ea746cddbe2eba4a0a3f43459be0d8e84823e
cemkata <cemkata@users.noreply.github.com>
2022-11-17 10:44:55 +0200

    file_orginiser.py is a module for folderTreeView

--- views/static/treemenu.js
+++ views/static/treemenu.js
@@ -1,58 +1,58 @@
-if (window.addEventListener) {              
-    window.addEventListener("resize", browserResize);
-} else if (window.attachEvent) {                 
-    window.attachEvent("onresize", browserResize);
+if (window.addEventListener) {
+	window.addEventListener("resize", browserResize);
+} else if (window.attachEvent) {
+	window.attachEvent("onresize", browserResize);
 }
 var xbeforeResize = window.innerWidth;
 function browserResize() {
-    var afterResize = window.innerWidth;
-    if ((xbeforeResize < (970) && afterResize >= (970)) || (xbeforeResize >= (970) && afterResize < (970)) ||
-        (xbeforeResize < (728) && afterResize >= (728)) || (xbeforeResize >= (728) && afterResize < (728)) ||
-        (xbeforeResize < (468) && afterResize >= (468)) ||(xbeforeResize >= (468) && afterResize < (468))) {
-        xbeforeResize = afterResize;
-    }
-    if (window.screen.availWidth <= 768) {
-      restack(window.innerHeight > window.innerWidth);
-    }
-    fixDragBtn();
+	var afterResize = window.innerWidth;
+	if ((xbeforeResize < (970) && afterResize >= (970)) || (xbeforeResize >= (970) && afterResize < (970)) ||
+		(xbeforeResize < (728) && afterResize >= (728)) || (xbeforeResize >= (728) && afterResize < (728)) ||
+		(xbeforeResize < (468) && afterResize >= (468)) ||(xbeforeResize >= (468) && afterResize < (468))) {
+		xbeforeResize = afterResize;
+	}
+	if (window.screen.availWidth <= 768) {
+		restack(window.innerHeight > window.innerWidth);
+	}
+	fixDragBtn();
 }
 
 var currentStack=true;
 if ((window.screen.availWidth <= 768 && window.innerHeight > window.innerWidth) || "" == " horizontal") {restack(true);}
 function restack(horizontal) {
-    var tc, ic, t, i, c, f, d, height, flt, width;
-    tc = document.getElementById("textareacontainer");
-    ic = document.getElementById("iframecontainer");
-    t = document.getElementById("textarea");
-    i = document.getElementById("iframe");
-    c = document.getElementById("container");    
-    tc.className = tc.className.replace("horizontal", "");
-    ic.className = ic.className.replace("horizontal", "");        
-    t.className = t.className.replace("horizontal", "");        
-    i.className = i.className.replace("horizontal", "");        
-    c.className = c.className.replace("horizontal", "");                        
-    stack = "";
-    if (horizontal) {
-        tc.className = tc.className + " horizontal";
-        ic.className = ic.className + " horizontal";        
-        t.className = t.className + " horizontal";        
-        i.className = i.className + " horizontal";                
-        c.className = c.className + " horizontal";                
+	var tc, ic, t, i, c, f, d, height, flt, width;
+	tc = document.getElementById("textareacontainer");
+	ic = document.getElementById("iframecontainer");
+	t = document.getElementById("textarea");
+	i = document.getElementById("iframe");
+	c = document.getElementById("container");
+	tc.className = tc.className.replace("horizontal", "");
+	ic.className = ic.className.replace("horizontal", "");
+	t.className = t.className.replace("horizontal", "");
+	i.className = i.className.replace("horizontal", "");
+	c.className = c.className.replace("horizontal", "");
+	stack = "";
+	if (horizontal) {
+		tc.className = tc.className + " horizontal";
+		ic.className = ic.className + " horizontal";
+		t.className = t.className + " horizontal";
+		i.className = i.className + " horizontal";
+		c.className = c.className + " horizontal";
 
-        stack = " horizontal";
-        document.getElementById("textareacontainer").style.height = "50%";
-        document.getElementById("iframecontainer").style.height = "50%";
-        document.getElementById("textareacontainer").style.width = "100%";
-        document.getElementById("iframecontainer").style.width = "100%";
-        currentStack=false;
-    } else {
-        document.getElementById("textareacontainer").style.height = "100%";
-        document.getElementById("iframecontainer").style.height = "100%";
-        document.getElementById("textareacontainer").style.width = "50%";
-        document.getElementById("iframecontainer").style.width = "50%";
-        currentStack=true;        
-    }
-    fixDragBtn();
+		stack = " horizontal";
+		document.getElementById("textareacontainer").style.height = "50%";
+		document.getElementById("iframecontainer").style.height = "50%";
+		document.getElementById("textareacontainer").style.width = "100%";
+		document.getElementById("iframecontainer").style.width = "100%";
+		currentStack=false;
+	} else {
+		document.getElementById("textareacontainer").style.height = "100%";
+		document.getElementById("iframecontainer").style.height = "100%";
+		document.getElementById("textareacontainer").style.width = "50%";
+		document.getElementById("iframecontainer").style.width = "50%";
+		currentStack=true;
+	}
+	fixDragBtn();
 }
 
 var dragging = false;
@@ -61,35 +61,35 @@ function fixDragBtn() {
   var textareawidth, leftpadding, dragleft, containertop, buttonwidth
   var containertop = Number(w3_getStyleValue(document.getElementById("container"), "top").replace("px", ""));
   if (stack != " horizontal") {
-    document.getElementById("dragbar").style.width = "5px";    
-    textareasize = Number(w3_getStyleValue(document.getElementById("textareawrapper"), "width").replace("px", ""));
-    leftpadding = Number(w3_getStyleValue(document.getElementById("textarea"), "padding-left").replace("px", ""));
-    buttonwidth = Number(w3_getStyleValue(document.getElementById("dragbar"), "width").replace("px", ""));
-    textareaheight = w3_getStyleValue(document.getElementById("textareawrapper"), "height");
-    dragleft = textareasize + leftpadding + (leftpadding / 2) - (buttonwidth / 2);
-    document.getElementById("dragbar").style.top = containertop + "px";
-    document.getElementById("dragbar").style.left = dragleft + "px";
-    document.getElementById("dragbar").style.height = textareaheight;
-    document.getElementById("dragbar").style.cursor = "col-resize";
-    
+	document.getElementById("dragbar").style.width = "5px";
+	textareasize = Number(w3_getStyleValue(document.getElementById("textareawrapper"), "width").replace("px", ""));
+	leftpadding = Number(w3_getStyleValue(document.getElementById("textarea"), "padding-left").replace("px", ""));
+	buttonwidth = Number(w3_getStyleValue(document.getElementById("dragbar"), "width").replace("px", ""));
+	textareaheight = w3_getStyleValue(document.getElementById("textareawrapper"), "height");
+	dragleft = textareasize + leftpadding + (leftpadding / 2) - (buttonwidth / 2);
+	document.getElementById("dragbar").style.top = containertop + "px";
+	document.getElementById("dragbar").style.left = dragleft + "px";
+	document.getElementById("dragbar").style.height = textareaheight;
+	document.getElementById("dragbar").style.cursor = "col-resize";
+
   } else {
-    document.getElementById("dragbar").style.height = "5px";
-    if (window.getComputedStyle) {
-        textareawidth = window.getComputedStyle(document.getElementById("textareawrapper"),null).getPropertyValue("height");
-        textareaheight = window.getComputedStyle(document.getElementById("textareawrapper"),null).getPropertyValue("width");
-        leftpadding = window.getComputedStyle(document.getElementById("textarea"),null).getPropertyValue("padding-top");
-        buttonwidth = window.getComputedStyle(document.getElementById("dragbar"),null).getPropertyValue("height");
-    } else {
-        dragleft = document.getElementById("textareawrapper").currentStyle["width"];
-    }
-    textareawidth = Number(textareawidth.replace("px", ""));
-    leftpadding = Number(leftpadding .replace("px", ""));
-    buttonwidth = Number(buttonwidth .replace("px", ""));
-    dragleft = containertop + textareawidth + leftpadding + (leftpadding / 2);
-    document.getElementById("dragbar").style.top = dragleft + "px";
-    document.getElementById("dragbar").style.left = "5px";
-    document.getElementById("dragbar").style.width = textareaheight;
-    document.getElementById("dragbar").style.cursor = "row-resize";        
+	document.getElementById("dragbar").style.height = "5px";
+	if (window.getComputedStyle) {
+		textareawidth = window.getComputedStyle(document.getElementById("textareawrapper"),null).getPropertyValue("height");
+		textareaheight = window.getComputedStyle(document.getElementById("textareawrapper"),null).getPropertyValue("width");
+		leftpadding = window.getComputedStyle(document.getElementById("textarea"),null).getPropertyValue("padding-top");
+		buttonwidth = window.getComputedStyle(document.getElementById("dragbar"),null).getPropertyValue("height");
+	} else {
+		dragleft = document.getElementById("textareawrapper").currentStyle["width"];
+	}
+	textareawidth = Number(textareawidth.replace("px", ""));
+	leftpadding = Number(leftpadding .replace("px", ""));
+	buttonwidth = Number(buttonwidth .replace("px", ""));
+	dragleft = containertop + textareawidth + leftpadding + (leftpadding / 2);
+	document.getElementById("dragbar").style.top = dragleft + "px";
+	document.getElementById("dragbar").style.left = "5px";
+	document.getElementById("dragbar").style.width = textareaheight;
+	document.getElementById("dragbar").style.cursor = "row-resize";
   }
 }
 function dragstart(e) {
@@ -99,25 +99,25 @@ function dragstart(e) {
 function dragmove(e) {
   if (dragging) 
   {
-    document.getElementById("shield").style.display = "block";
-    if (stack != " horizontal") {
-      var percentage = (e.pageX / window.innerWidth) * 100;
-      if (percentage > 15 && percentage < 75) {
-        var mainPercentage = 100-percentage;
-        document.getElementById("textareacontainer").style.width = percentage + "%";
-        document.getElementById("iframecontainer").style.width = mainPercentage + "%";
-        fixDragBtn();
-      }
-    } else {
-      var containertop = Number(w3_getStyleValue(document.getElementById("container"), "top").replace("px", ""));
-      var percentage = ((e.pageY - containertop + 20) / (window.innerHeight - containertop + 20)) * 100;
-      if (percentage > 15 && percentage < 75) {
-        var mainPercentage = 100-percentage;
-        document.getElementById("textareacontainer").style.height = percentage + "%";
-        document.getElementById("iframecontainer").style.height = mainPercentage + "%";
-        fixDragBtn();
-      }
-    }   
+	document.getElementById("shield").style.display = "block";
+	if (stack != " horizontal") {
+	  var percentage = (e.pageX / window.innerWidth) * 100;
+	  if (percentage > 15 && percentage < 75) {
+		var mainPercentage = 100-percentage;
+		document.getElementById("textareacontainer").style.width = percentage + "%";
+		document.getElementById("iframecontainer").style.width = mainPercentage + "%";
+		fixDragBtn();
+	  }
+	} else {
+	  var containertop = Number(w3_getStyleValue(document.getElementById("container"), "top").replace("px", ""));
+	  var percentage = ((e.pageY - containertop + 20) / (window.innerHeight - containertop + 20)) * 100;
+	  if (percentage > 15 && percentage < 75) {
+		var mainPercentage = 100-percentage;
+		document.getElementById("textareacontainer").style.height = percentage + "%";
+		document.getElementById("iframecontainer").style.height = mainPercentage + "%";
+		fixDragBtn();
+	  }
+	}
   }
 }
 function dragend() {
@@ -134,11 +134,11 @@ if (window.addEventListener) {
 }
 
 function w3_getStyleValue(elmnt,style) {
-    if (window.getComputedStyle) {
-        return window.getComputedStyle(elmnt,null).getPropertyValue(style);
-    } else {
-        return elmnt.currentStyle[style];
-    }
+	if (window.getComputedStyle) {
+		return window.getComputedStyle(elmnt,null).getPropertyValue(style);
+	} else {
+		return elmnt.currentStyle[style];
+	}
 }
 
 browserResize();
@@ -221,7 +221,7 @@ function addleaf(root, leaf, path){
 			root.insertBefore(li, root.firstChild);
 			root = root.firstChild;
 		}
-		
+
 		root.appendChild(ul);
 		if(leaf.children.length != 0){
 			for(let i = 0; i < leaf.children.length; i++){
@@ -262,9 +262,9 @@ function search(){
 	links = searchResultsHolder.getElementsByTagName('a');
 	totalNumberOfLinks = links.length; //Length chenges on each iteration and we store the array length 
 	for(let i = 1; i < totalNumberOfLinks; i++){
-		links[1].remove() //remove the second link. First is the close button
+		links[1].remove(); //remove the second link. First is the close button
 	}
-	
+
 	//Fill the result holder
 	for(let i = 0; i < searchResult.length; i++){
 		var a = document.createElement('a');
@@ -272,11 +272,14 @@ function search(){
 		let tmp = searchResult[i].split('/');
 		a.innerHTML = tmp[tmp.length-1];
 		a.addEventListener("click", function() {
-			pdfView.src = treeValues.name + "/" + searchResult[i];	
+			pdfView.src = treeValues.name + "/" + searchResult[i];
+			foldTree();
+			unselectLeaf();
+			openLeaf(searchResult[i]);
 		});
-		searchResultsHolder.appendChild(a)
+		searchResultsHolder.appendChild(a);
 	}
-	openNav()
+	openNav();
 }
 
 //var variable = 'foo';
@@ -286,17 +289,17 @@ function search(){
 //re.test('fdjklsfdjkslfd')   // false
 
 function searchInJSON(re, jsonContainer){
-    var result = [];
+	var result = [];
 	for(let i = 0; i < jsonContainer.children.length; i++){
 		if(jsonContainer.children[i].type=="file"){
 			if (re.test(jsonContainer.children[i].name)) {
-			    result.push(jsonContainer.children[i].name)
+				result.push(jsonContainer.children[i].name)
 				//console.log(jsonContainer.children[i].name)
 			}
 		}else{
 			value = searchInJSON(re, jsonContainer.children[i]);
 			if(value){
-			    if(value.length > 0){
+				if(value.length > 0){
 					for(let k = 0; k < value.length; k++){
 						result.push(jsonContainer.children[i].name + '/' + value[k]);
 					}
@@ -327,7 +330,12 @@ document.getElementById('searchinput').addEventListener('input', (e) => {
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  searchResultsHolder.style.width = "250px";
+  //searchResultsHolder.style.width = "250px";
+  if(currentStack){
+		searchResultsHolder.style.width = "25%";
+  }else{
+		searchResultsHolder.style.width = "100%";
+  }
 }
 
 /* Set the width of the side navigation to 0 */
@@ -336,4 +344,63 @@ function closeNav() {
   searchField.value = "";
 }
 
-console.log("treemenu version 2.3")
+function foldTree(){
+	x = document.querySelectorAll('.caret-down');
+	y = document.querySelectorAll('.active');
+
+	for(let i = 1; i< x.length; i++){
+		x[i].classList.toggle("caret-down");
+	/*}
+	for(let i = 1; i< y.length; i++){*/
+		y[i].classList.toggle("active");
+	}
+}
+
+function unselectLeaf(){
+	z = document.querySelectorAll('.selected');
+	for(let i = 0; i< z.length; i++){
+		z[i].classList.toggle("selected");
+	}
+}
+
+function openLeaf(inString){
+	var tmpStrings = inString.split("/");
+	//var exitFlag = true;
+	var j = 0;
+	var curentLeaf = treeMenu.childNodes[0].children[1].children;
+	while(j != tmpStrings.length){
+	/*while(exitFlag){
+		if(j == tmpStrings.length){
+			exitFlag = false
+			continue;
+		}*/
+		for(let i = 0; i<curentLeaf.length; i++){
+			if(curentLeaf[i].children[0].textContent === tmpStrings[j]){
+				try {
+					curentLeaf[i].children[1].classList.toggle("active");
+					curentLeaf[i].children[0].classList.toggle("caret-down");
+					curentLeaf = curentLeaf[i].children[1].children;
+				} catch (exceptionVar) {
+					curentLeaf[i].children[0].classList.toggle("selected");
+				}finally {
+					break;
+				}
+			}
+		}
+		j++;
+	}
+}
+
+file_organaser_url = "./files/"
+
+document.getElementById("logo").addEventListener("click", function(event) {
+   /*if(event.ctrlKey)
+      console.log('ctrl');
+   if(event.altKey)
+      console.log('alt');*/
+  if(event.ctrlKey && event.altKey){
+	  window.open(file_organaser_url, '_blank').focus();
+  }
+});
+
+console.log("treemenu version 3.3")
